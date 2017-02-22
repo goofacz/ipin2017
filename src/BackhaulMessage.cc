8f88a0ae19dbdea45e23b679f18981bc789737d6
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2017-02-22 21:47:04 +0100

    Extend BackhaulMessage
    Signed-off-by: Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>

---
+++ src/BackhaulMessage.cc
@@ -0,0 +1,63 @@
+#include "BackhaulMessage.h"
+
+using namespace omnetpp;
+
+namespace ipin2017
+{
+
+Register_Class(BackhaulMessage);
+
+BackhaulMessage::BackhaulMessage (const char *name, int kind) :
+    BackhaulMessage_Base (name, kind)
+{
+    // ...
+}
+
+BackhaulMessage::BackhaulMessage (const BackhaulMessage& other) :
+    BackhaulMessage_Base (other)
+{
+    frame = nullptr;
+    copy (other);
+}
+
+BackhaulMessage::~BackhaulMessage ()
+{
+    if (frame)  {
+        delete frame;
+    }
+}
+
+BackhaulMessage&
+BackhaulMessage::operator = (const BackhaulMessage& other)
+{
+    if (this == &other) {
+        return *this;
+    }
+    
+    BackhaulMessage_Base::operator = (other);
+    copy (other);
+
+    return *this;
+}
+
+BackhaulMessage*
+BackhaulMessage::dup() const
+{
+    return new BackhaulMessage (*this);
+}
+
+void
+BackhaulMessage::copy (const BackhaulMessage& other)
+{
+    if (frame)
+    {
+        delete frame;
+        frame = nullptr;
+    }
+
+    if (other.frame)    {
+        frame = other.frame->dup ();
+    }
+}
+
+} // namespace ipin2017
---
+++ src/BackhaulMessage.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "BackhaulMessage_m.h"
+
+namespace ipin2017
+{
+
+class BackhaulMessage : public BackhaulMessage_Base
+{
+    public:
+        BackhaulMessage (const char *name=nullptr, int kind=0);
+
+        BackhaulMessage (const BackhaulMessage& other);
+
+        ~BackhaulMessage ();
+
+        BackhaulMessage& operator = (const BackhaulMessage& other);
+
+        BackhaulMessage* dup () const override;
+
+    private:
+        void copy (const BackhaulMessage& other);
+};
+
+} // namespace ipin2017
