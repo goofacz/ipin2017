1b5c7b0a1eec8db60ec89dd49bac39dbc5f998a9
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2017-04-24 22:12:32 +0200

    Update Matlab scripts.
    Signed-off-by: Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>

--- matlab/computeArticleData.m
+++ matlab/computeArticleData.m
@@ -1,6 +1,4 @@
-function [absPosErr, ...
-          absPosErrMin,absPosErrMax, ...
-          absPosErrAvg,absPosErrStdDev] = computeArticleData(results)
+function [absPosErrs, absErrStats] = computeArticleData(results)
 
 % Columns
 COORD_X = 1;
@@ -9,18 +7,15 @@ REAL_COORD_X = 3;
 REAL_COORD_Y = 4;
 
 % Variables
-absPosErr = zeros(size(results,1),1);
+absPosErrs = zeros(size(results,1),1);
 
 % Computations
 for i = 1:size(results,1)
-    absPosErr(i) = pdist([results(i,COORD_X) results(i,COORD_Y);
-                          results(i,REAL_COORD_X) results(i,REAL_COORD_Y)],'euclidean');
+    absPosErrs(i) = pdist([results(i,COORD_X) results(i,COORD_Y);
+                           results(i,REAL_COORD_X) results(i,REAL_COORD_Y)],'euclidean');
 end
 
-absPosErrMin = min(absPosErr);
-absPosErrMax = max(absPosErr);
-absPosErrAvg = mean(absPosErr);
-absPosErrStdDev = std(absPosErr);
+absErrStats = [min(absPosErrs), max(absPosErrs), mean(absPosErrs), std(absPosErrs)];
 
 end
 
