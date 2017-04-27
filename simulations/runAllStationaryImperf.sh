f0f05ece6b6c30efbd3b2a5b773fac13ad32d246
Tomasz Jankowski <t.jankowski@pwr.edu.pl>
2017-04-27 16:49:09 +0200

    Final fixes in tdoa.ini and new scripts running simulations.

---
+++ simulations/runAllStationaryImperf.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+opp_runall -j5 ../src/ipin2017 -u Cmdenv -r 0..10000 -n ../src:.:../../inet/examples:../../inet/src:../../inet/tutorials -l ../../inet/src/INET --debug-on-errors=false tdoa.ini -c TDoA_stationary_imperfect_clock
