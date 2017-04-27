1651a0d126b6a8319709301532761f428bcb3a6f
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2017-04-27 23:22:37 +0200

    Final cleanup in simulation configuration.
    Signed-off-by: Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>

--- simulations/runAllStationaryImperf.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-opp_runall -j5 ../src/ipin2017 -u Cmdenv -r 0..10000 -n ../src:.:../../inet/examples:../../inet/src:../../inet/tutorials -l ../../inet/src/INET --debug-on-errors=false tdoa.ini -c TDoA_stationary_imperfect_clock
---
+++ simulations/runWhistleStationaryPerf.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+opp_runall -j5 ../src/ipin2017 -u Cmdenv -r 0..10000 -n ../src:.:../../inet/examples:../../inet/src:../../inet/tutorials -l ../../inet/src/INET --debug-on-errors=false omnet.ini -c Whistle_stationary_perfect_clock
