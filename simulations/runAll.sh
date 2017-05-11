0e07d8a846021fceb37c00be95fa425952d06e80
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2017-05-11 15:25:47 +0200

    Add runAll.sh
    Signed-off-by: Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>

---
+++ simulations/runAll.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+./runTDoAStationaryImperf.sh
+./runTDoAStationaryPerf.sh
+./runTDoAMovingImperf.sh
+./runTDoAMovingPerf.sh
+#./runWhistleStationaryImperf.sh
+./runWhistleStationaryPerf.sh
+./runWhistleMovingImperf.sh
+./runWhistleMovingPerf.sh
+
