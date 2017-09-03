e3ba3775d81013955ad90d6ff88412004ce434b3
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2017-09-03 22:32:49 +0200

    Add copyleft to Matlab scripts.

--- matlab/whistleProcessSimulationResults.m
+++ matlab/whistleProcessSimulationResults.m
@@ -4,6 +4,24 @@ function positions = whistleProcessSimulationResults(filePath)
 ANCHOR_ADDR = 1;
 IS_ECHO_FRAME = 2; % 1 - replied frame, 0 - original frame
 SEQ_NO = 3;
+%   IPIN 2017 Localization Method Simulator
+% 
+%   Copyright (C) 2017 Tomasz Jankowski
+%   
+%   This program is free software; you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation; either version 3 of the License, or
+%   (at your option) any later version.
+%      
+%   This program is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+%      
+%   You should have received a copy of the GNU General Public License
+%   along with this program; if not, write to the Free Software Foundation,
+%   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+
 RX_TS = 4;
 TX_TS = 5;
 ANCH_COORD_X = 6;
